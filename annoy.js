5b3881d2d92e8e3ffb2c66fdc001d8cfe36ce996
Josephamalalincy <72212339+Josephamalalincy@users.noreply.github.com>
2020-10-01 21:25:03 +0530

    Add files via upload

---
+++ annoy.js
@@ -0,0 +1,22 @@
+ //var answer = prompt("are we there yet");
+
+//while(answer !== "yes" && answer !== "yeah") {
+	//var answer = prompt("are we there yet");
+//}
+
+//alert("yah we made it!!!");
+
+
+//if(answer === "yes"){
+//	alert("yay,we made it")
+//} else {
+//}
+
+//version 2
+
+var answer = prompt("are we there yet");
+
+while(answer.index0f("yes") === -1) {
+	var answer = prompt("are we there yer");
+}
+alert("yay we made it!!!);
